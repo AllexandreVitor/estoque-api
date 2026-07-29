7a5658d36e2e747092da900802a811985eb27025
Alexandre Vitor <allexandrevitor.av@gmail.com>
2026-07-29 15:40:48 -0300

    feat: integra endpoint de status do estoque

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userRoutes = require('./src/routes/userRoutes'); // NOVA LINHA
 const app = express();
+const estoqueRoutes = require('./src/routes/estoqueRoutes'); // Adicione as linhas referentes ao estoque
 
 app.use(express.json());
 app.use('/api/usuarios', userRoutes); // NOVA LINHA
+app.use('/api/estoque', estoqueRoutes); //NOVA LINHA
 
 app.listen(3000, () => console.log('API rodando na porta 3000'));
